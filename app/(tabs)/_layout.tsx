8cb1e896e504070eccf9710650d897e4e607a2c5
Safet <safet.srna@fpmoz.sum.ba>
2026-05-08 10:41:49 +0200

    8.5.2026

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -38,7 +38,6 @@ export default function TabLayout() {
                     tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
                 }}
             />
-
             <Tabs.Screen
                 name="games"
                 options={{
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,24 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
 import { AuthProvider } from "@/contexts/AuthContext";
-
-export const unstable_settings = {
-    anchor: '(tabs)',
-};
 
 export default function RootLayout() {
     const colorScheme = useColorScheme();
 
     return (
-        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
             <AuthProvider>
                 <Stack>
-                    <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
-                    <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+                    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                    <Stack.Screen name="game-one" options={{ title: "Game One" }} />
+                    <Stack.Screen
+                        name="modal"
+                        options={{ presentation: "modal", title: "Modal" }}
+                    />
                 </Stack>
                 <StatusBar style="auto" />
             </AuthProvider>
